07495b635d83c87afbb48a9ddf03bd1e56dba501
RyanLin5967 <idident5967@gmail.com>
2026-02-19 00:02:43 -0500

    actual final edit

--- src/main/java/com/ryanlin/remapper/HeatmapManager.java
+++ src/main/java/com/ryanlin/remapper/HeatmapManager.java
@@ -1,11 +1,7 @@
 package com.ryanlin.remapper;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.Collections;
 
 public class HeatmapManager {
     
